7eab6f61b5c1b1b9f7f6ebb7100e512e985cb6bf
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2024-09-23 16:59:55 -0500

    b

--- LaboratorioDos/src/main/java/com/example/laboratoriodos/Idioma.java
+++ LaboratorioDos/src/main/java/com/example/laboratoriodos/Idioma.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
